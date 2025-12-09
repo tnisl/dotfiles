732e98ba29adfde1899fb366c2cdaf468e048120
tnisl <memedead123@gmail.com>
2025-12-09 14:40:10 +0700

    update lightctl function

--- modules/home-manager/scripts/lightctl/default.nix
+++ modules/home-manager/scripts/lightctl/default.nix
@@ -6,23 +6,34 @@ let
     
 
     state=$1
-    
-    
+
+
+
+    cur=$(${pkgs.brightnessctl}/bin/brightnessctl g)
+    max=$(${pkgs.brightnessctl}/bin/brightnessctl m)
+
+    percentage=$(($cur * 100 / $max))
     if [ "$state" = "up" ]; then
       ${pkgs.brightnessctl}/bin/brightnessctl -q s +2%
     elif [ "$state" = "down" ]; then
-      ${pkgs.brightnessctl}/bin/brightnessctl -q s 2%-
+      
+
+      next=$(($percentage - 2))
+      
+      if [ $next -lt 5 ]; then
+        ${pkgs.brightnessctl}/bin/brightnessctl -q s 5%
+      else
+        ${pkgs.brightnessctl}/bin/brightnessctl -q s 2%-
+      fi
     fi
     
     cur=$(${pkgs.brightnessctl}/bin/brightnessctl g)
-    
-    max=$(${pkgs.brightnessctl}/bin/brightnessctl m)
-    
     percentage=$(($cur*100/$max))
     
     ${pkgs.libnotify}/bin/notify-send "Brightness: $percentage%" -t 3000 -h string:x-canonical-private-synchronous:brightness -h int:value:$percentage
 
     '';
+  
 
 in
 {
