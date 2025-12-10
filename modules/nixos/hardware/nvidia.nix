0d979f8c530199a123fc5f529f289e79bad09105
tnisl <memedead123@gmail.com>
2025-12-10 21:24:22 +0700

    fix dgpu

--- modules/nixos/hardware/nvidia.nix
+++ modules/nixos/hardware/nvidia.nix
@@ -4,18 +4,18 @@
 
 	nvidia = {
   		modesetting.enable = false;
-        	powerManagement.enable = false;
-        	powerManagement.finegrained = false;
+        	powerManagement.enable = true;
+        	powerManagement.finegrained = true;
         	open = false;
         	nvidiaSettings = false;
         	package = config.boot.kernelPackages.nvidiaPackages.stable;
         	
-        	prime = {
-			offload.enable = true;
-			offload.enableOffloadCmd = true;
-        		intelBusId = "PCI:0:2:0";
-        		nvidiaBusId = "PCI:1:0:0";
-        	};
+      prime = {
+      offload.enable = true;
+      offload.enableOffloadCmd = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+      };
 
   	};
 
