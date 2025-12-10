1f60d7393b4894741aacaa7b2eccb5d33c495053
tnisl <memedead123@gmail.com>
2025-12-10 21:22:13 +0700

    add editor opener

--- modules/home-manager/configs/yazi/default.nix
+++ modules/home-manager/configs/yazi/default.nix
@@ -1,6 +1,44 @@
 {pkgs, ...}:
 {
 
-  home.packages = with pkgs; [yazi];
+
+
+  programs.yazi = {
+    enable = true;
+    settings = {
+        # 1. Định nghĩa hành động (Opener)
+        opener = {
+          # Đặt tên opener là "wall"
+
+          edit = [
+          {
+
+            run = '' $EDITOR "$@" '';
+            block = true;
+            desc = "Edit file";
+          }
+
+
+
+        ];
+          wall = [
+            {
+              run = ''${pkgs.swww}/bin/swww img "$1"'';
+              orphan = true;
+              desc = "Set as Wallpaper";
+            }
+          ];
+        };
+    
+        open = {
+          rules = [
+            { mime = "image/*"; use = [ "open" "wall" ]; }
+            { mime = "text/*"; use = [ "edit" ]; }
+          ];
+        };
+      };
+
+
+  };
 
 }
