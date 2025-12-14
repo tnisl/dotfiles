0d1b657bd595be4ecd62931eec0d70a36e1d53e6
tnisl <memedead123@gmail.com>
2025-12-15 05:54:21 +0700

    fix telescope

--- modules/home-manager/configs/neovim/plugins/telescope.nix
+++ modules/home-manager/configs/neovim/plugins/telescope.nix
@@ -1,19 +1,18 @@
 {pkgs, ...}:
 {
   programs.neovim.plugins = with pkgs.vimPlugins; [
-      {
-        plugin = telescope-nvim;
-        type = "lua";
-        config = ''
+      telescope-nvim
+      telescope-fzf-native-nvim
+  ];
+
+  programs.neovim.extraLuaConfig = ''
+
+    vim.g.mapleader = " "
     local builtin = require("telescope.builtin")
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', {})
-        '';
-      }
-      telescope-fzf-native-nvim
-
-  ];
+    -- vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', {})
+  '';
 
 
 }
