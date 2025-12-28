707555e4ce44ac0c9137e38069949077645cf2e4
tnisl <memedead123@gmail.com>
2025-12-28 21:23:53 +0700

    quick fix

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -1,27 +1,9 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 
 
 {
   imports = [
-    ##sway
-    #../../modules/home-manager/sway/default.nix
-    ##hyprland
-		#../../modules/home-manager/hyprland/default.nix
-		##packages
-		#../../modules/home-manager/packages/ricing/default.nix
-		#../../modules/home-manager/packages/work/default.nix
-		#../../modules/home-manager/packages/apps/default.nix
-		#../../modules/home-manager/packages/hypr-eco/default.nix
-		#../../modules/home-manager/packages/tool/default.nix
-		##services
-		#../../modules/home-manager/services/default.nix
-		##configs
-		#../../modules/home-manager/configs/default.nix
-
-		##scripts
-		#../../modules/home-manager/scripts/default.nix
-
     ../../modules/home-manager/default.nix
   ];
 
@@ -35,9 +17,7 @@
     stateVersion = "24.05";
     sessionVariables = {
       TERMINAL = "kitty";
-
       TERM = "xterm-256color";
-
     };
   };
 
